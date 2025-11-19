a583f5ad24b2676f15ff4651725ab6a33a4756ed
Baseone <deltabaseoneartha@gmail.com>
2025-11-19 19:40:05 +0700

    fix: rename Public to public

--- .next/static/webpack/webpack.3d9cfdce6eced222.hot-update.js
+++ .next/static/webpack/webpack.3d9cfdce6eced222.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "742e9eba1298ef04"; }
+/******/ 	__webpack_require__.h = function() { return "87991153eea99aaf"; }
 /******/ }();
 /******/ 
 /******/ }
