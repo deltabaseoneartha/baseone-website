56b909d85539db001d78ffd35aad3ad9263c58b1
Baseone <deltabaseoneartha@gmail.com>
2025-11-19 21:11:50 +0700

    fix config

--- Next.config.js
+++ Next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
+  reactStrictMode: true,
   images: {
-    unoptimized: true
-  }
-}
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
